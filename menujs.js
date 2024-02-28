5607dfda56e2b445c51d8db6cf088d98882850dd
Amilton Gove <155755000+amiltongovegove@users.noreply.github.com>
2024-02-28 21:15:15 +0200

    Update menujs.js

--- menujs.js
+++ menujs.js
@@ -1,7 +1,7 @@
             function clickCategorias() {
 			  if (menu.style.display == 'block') {
 			     menu.style.display = 'none'
-				 } else {
+			 } else {
 				 menu.style.display = 'block'
 			  }
 			  }
@@ -9,23 +9,16 @@
 			function clickSlide() {
 			  if (xslide.style.display == 'block') {
 			     xslide.style.display = 'none'
-				 } else {
+			 } else {
 				 xslide.style.display = 'block'
 			  }
 			  } 
 		    
-			 
-            function calc_total() {
-                 var qtd = parseInt(document.getElementById('cQtd').value);
-                 tot = qtd * 100000;
-                 document.getElementById('cTot').value = tot;
-             }
-			 
-			 function alertNotice() {
+			function alertNotice() {
 			     alert("Esse site é uma comunidade, com o objectivo de te manter informado sobre os jogos onlines");
 			 }
 			 
 			 
-			 function istDow() {
+			function istDow() {
 				 alert("As instalaçoes Só estao disponiveis para dispositivos Mobiles");
 			 }

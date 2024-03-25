96bb14ed48c327f6f7bb929b8d28a7d53461fd83
Amilton Gove <155755000+amiltongovegove@users.noreply.github.com>
2024-03-25 10:57:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+//Menu Responsivo
+let a = window.document.getElementsByClassName('itens')[0]
 function menu() {
-    let a = window.document.getElementById('itens')
     if (a.style.display == 'block') {
         a.style.display = 'none'
     } else { 
        a.style.display = 'block'
+    } 
+}
+function move() {
+    if (a.style.display == 'none') {
+        a.style.display = 'flex'
     }
-
 }

38dc3961e0103178d1121d86e90029bef52945bf
Amilton Gove <155755000+amiltongovegove@users.noreply.github.com>
2024-03-25 09:30:51 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function menu() {
+    let a = window.document.getElementById('itens')
+    if (a.style.display == 'block') {
+        a.style.display = 'none'
+    } else { 
+       a.style.display = 'block'
+    }
+
+}
